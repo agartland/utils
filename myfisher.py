3aa35b2d38fe6339143be33ba93d26e6447aef8a
Andrew Gartland <agartland@gmail.com>
2014-09-29 17:28:32 -0700

    Adding files to the repo

---
+++ myfisher.py
@@ -0,0 +1,22 @@
+from __future__ import division
+
+__all__ = ['fisherTest']
+
+try:
+    """Attempt to use the fisher library (cython) if available (100x speedup)"""
+    import fisher
+    def fisherTest(tab,alternative='two-sided'):
+        res = fisher.pvalue(tab[0][0],tab[0][1],tab[1][0],tab[1][1])
+        OR = (tab[0][0] * tab[1][1]) / (tab[0][1] * tab[1][0])
+
+        if alternative == 'two-sided':
+            return (OR,res.two_tail)
+        elif alternative == 'less':
+            return (OR,res.left_tail)
+        elif alternative == 'greater':
+            return (OR,res.right_tail)
+    print "Using Cython-powered Fisher's exact test"
+except ImportError:
+    from scipy import stats
+    print "Using scipy.stats Fisher's exact test (slow)"
+    fisherTest = stats.fisher_exact
