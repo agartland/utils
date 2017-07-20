9754713c5b0f8d4f006522ac983f15be5ae55d50
agartland <agartland@gmail.com>
2017-07-20 14:39:16 -0700

    Added SE functions to imm_cor

--- imm_cor.py
+++ imm_cor.py
@@ -10,7 +10,9 @@ __all__ = ['estCumTE',
             'estCoxPHTE',
             'scoreci',
             'AgrestiScoreVE',
-            'unconditionalVE']
+            'unconditionalVE',
+            'diffscoreci',
+            'riskscoreci']
 
 def estCumTE(df, treatment_col='treated', duration_col='dx', event_col='disease', followupT=None, alpha=0.05, H1=0, bootstraps=None):
     """Estimates treatment efficacy using cumulative incidence (CI) Nelson-Aalen (NA) estimators.
@@ -320,3 +322,220 @@ def AgrestiScoreVE(nv,Nv, np, Np, alpha=0.025):
     p = stats.binom.cdf(nv, n, Nv/(Nv+Np))
     
     return  pd.Series([ve, ci[0], ci[1], p], index=['VE', 'LL', 'UL', 'p'])
+
+
+def riskscoreci(x1,n1,x2,n2,conf_level=0.95):
+    """Compute CI for the ratio of two binomial rates.
+    
+    Translated from R-package 'PropCIs':
+    https://github.com/shearer/PropCIs
+
+    Nam, J. M. (1995) Confidence limits for the ratio of two binomial proportions based on likelihood
+    scores: Non-iterative method. Biom. J. 37 (3), 375-379.
+    
+    Koopman PAR. (1985) Confidence limits for the ratio of two binomial proportions. Biometrics 40,
+    513-517.
+    
+    Miettinen OS, Nurminen M. (1985) Comparative analysis of two rates. Statistics in Medicine 4,
+    213-226.
+    
+    Nurminen, M. (1986) Analysis of trends in proportions with an ordinally scaled determinant. Biometrical
+    J 28, 965-974
+    
+    Agresti, A. (2002) Categorical Data Analysis. Wiley, 2nd Edition.
+
+    Parameters
+    ----------
+    xi : int
+        Number of events in group i
+    ni : int
+        Number of trials/subjects in group i
+    conf_level : float
+        Specifies coverage of the confidence interval (1 - alpha)
+
+    Returns
+    -------
+    ci : array
+        Confidence interval array [LL, UL]"""
+
+    z =  abs(stats.norm.ppf((1-conf_level)/2))
+    if x2==0 and x1 == 0:
+        ul = np.inf
+        ll = 0
+    else:
+        a1 =  n2*(n2*(n2+n1)*x1+n1*(n2+x1)*(z**2))
+        a2 = -n2*(n2*n1*(x2+x1)+2*(n2+n1)*x2*x1+n1*(n2+x2+2*x1)*(z**2))
+        a3 = 2*n2*n1*x2*(x2+x1)+(n2+n1)*(x2**2)*x1+n2*n1*(x2+x1)*(z**2)
+        a4 = -n1*(x2**2)*(x2+x1)
+        b1 = a2/a1
+        b2 = a3/a1
+        b3 = a4/a1
+        c1 = b2-(b1**2)/3
+        c2 = b3-b1*b2/3+2*(b1**3)/27
+        ceta = np.arccos(np.sqrt(27)*c2/(2*c1*np.sqrt(-c1)))
+        t1 = -2*np.sqrt(-c1/3)*np.cos(np.pi/3-ceta/3)
+        t2 = -2*np.sqrt(-c1/3)*np.cos(np.pi/3+ceta/3)
+        t3 = 2*np.sqrt(-c1/3)*np.cos(ceta/3)
+        p01 = t1-b1/3
+        p02 = t2-b1/3
+        p03 = t3-b1/3
+        p0sum = p01+p02+p03
+        p0up = min(p01,p02,p03)
+        p0low = p0sum-p0up-max(p01,p02,p03)
+
+        if x2 == 0 and x1 != 0:
+            ll = (1-(n1-x1)*(1-p0low)/(x2+n1-(n2+n1)*p0low))/p0low
+            ul = np.inf
+        elif x2 != n2 and x1==0:
+            ul = (1-(n1-x1)*(1-p0up)/(x2+n1-(n2+n1)*p0up))/p0up
+            ll = 0
+        elif x2 == n2 and x1 == n1:
+            ul = (n2+z**2)/n2
+            ll =  n1/(n1+z**2)
+        elif x1 == n1 or x2 == n2:
+            if x2 == n2 and x1 == 0:
+                ll = 0
+            if x2 == n2 and x1 != 0:
+                phat1  = x2/n2
+                phat2  =  x1/n1
+                phihat = phat2/phat1
+                phil = 0.95*phihat
+                chi2 = 0
+                while chi2 <= z:
+                    a = (n2+n1)*phil
+                    b = -((x2+n1)*phil+x1+n2)
+                    c = x2+x1
+                    p1hat = (-b-np.sqrt(b**2-4*a*c))/(2*a)
+                    p2hat = p1hat*phil
+                    q2hat = 1-p2hat
+                    var = (n2*n1*p2hat)/(n1*(phil-p2hat)+n2*q2hat)
+                    chi2 = ((x1-n1*p2hat)/q2hat)/np.sqrt(var)
+                    ll = phil
+                    phil = ll/1.0001
+            i = x2
+            j = x1
+            ni = n2
+            nj = n1
+            if x1 == n1:
+                i = x1
+                j = x2
+                ni = n1
+                nj = n2
+            
+            phat1  = i/ni
+            phat2  =  j/nj
+            phihat = phat2/phat1
+            phiu = 1.1*phihat
+            if x2 == n2 and x1 == 0:
+                if n2<100:
+                    phiu = .01
+                else:
+                    phiu = 0.001
+            chi1 = 0
+            while chi1 >= -z:
+                a = (ni+nj)*phiu
+                b = -((i+nj)*phiu+j+ni)
+                c = i+j
+                p1hat = (-b-np.sqrt(b**2-4*a*c))/(2*a)
+                p2hat = p1hat*phiu
+                q2hat = 1-p2hat
+                var = (ni*nj*p2hat)/(nj*(phiu-p2hat)+ni*q2hat)
+                chi1  = ((j-nj*p2hat)/q2hat)/np.sqrt(var)
+                phiu1 = phiu
+                phiu = 1.0001*phiu1
+
+            if x1 == n1:
+                ul = (1-(n1-x1)*(1-p0up)/(x2+n1-(n2+n1)*p0up))/p0up
+                ll = 1/phiu1
+            else:
+                ul = phiu1
+        else:
+            ul = (1-(n1-x1)*(1-p0up)/(x2+n1-(n2+n1)*p0up))/p0up
+            ll = (1-(n1-x1)*(1-p0low)/(x2+n1-(n2+n1)*p0low))/p0low
+    return np.array([ll, ul])
+
+def diffscoreci(x1,n1,x2,n2,conf_level):
+    """Score interval for difference in proportions
+    
+    Method of Mee 1984 with Miettinen and Nurminen modification nxy / (nxy - 1), see Newcombe 1998
+
+    Agresti, A. (2002) Categorical Data Analysis. Wiley, 2nd Edition.
+    
+    Mee, RW. (1984) Confidence bounds for the difference between two probabilities. Biometrics 40,
+    1175-1176.
+    
+    Miettinen OS, Nurminen M. (1985) Comparative analysis of two rates. Statistics in Medicine 4,
+    213-226.
+    
+    Nurminen, M. (1986) Analysis of trends in proportions with an ordinally scaled determinant. Biometrical
+    J. 28, 965-974
+
+    Parameters
+    ----------
+    xi : int
+        Number of events in group i
+    ni : int
+        Number of trials/subjects in group i
+    conf_level : float
+        Specifies coverage of the confidence interval (1 - alpha)
+
+    Returns
+    -------
+    ci : array
+        Confidence interval array [LL, UL]"""
+   
+    px = x1/n1
+    py = x2/n2
+    z = stats.chi2.ppf(conf_level,1)
+    proot = px - py
+    dp = 1 - proot
+    niter = 1
+    while niter <= 50:
+        dp = 0.5 * dp
+        up2 = proot + dp
+        score = _z2stat(px,n1,py,n2,up2)
+        if score < z:
+            proot = up2
+        niter = niter + 1
+        if dp<0.0000001 or abs(z-score) < 0.000001:
+            niter = 51
+            ul = up2
+    proot = px - py
+    dp = 1 + proot
+    niter = 1
+    while niter <= 50:
+        dp = 0.5 * dp
+        low2 = proot - dp
+        score = _z2stat(px,n1,py,n2,low2)
+        if score < z:
+            proot = low2
+        niter = niter+1
+        if dp<0.0000001 or abs(z-score)<0.000001:
+            ll = low2
+            niter = 51
+    return np.array([ll, ul])
+
+def _z2stat(p1x,nx,p1y,ny,dif):
+    """Private function used by diffscoreci"""
+    difference = p1x-p1y-dif
+    if abs(difference) == 0:
+        fmdifference = 0
+    else:
+        t = ny/nx
+        a = 1+t
+        b = -(1+ t + p1x + t*p1y + dif*(t+2))
+        c = dif*dif + dif*(2*p1x + t +1) + p1x + t*p1y
+        d = -p1x*dif*(1+dif)
+        v = (b/a/3)**3 - b*c/(6*a*a) + d/a/2
+        s = np.sqrt( (b/a/3)**2 - c/a/3)
+        if v>0:
+            u = s
+        else:
+            u = -s
+        w = (np.pi + np.arccos(v/u**3))/3
+        p1d = 2*u*np.cos(w) - b/a/3
+        p2d = p1d - dif
+        nxy = nx + ny
+        var = (p1d*(1-p1d)/nx + p2d*(1-p2d)/ny) * nxy / (nxy - 1) ## added: * nxy / (nxy - 1)
+        fmdifference = difference**2/var
+    return fmdifference
