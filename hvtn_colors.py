8cfba736776c26a3083c88786b92e0f5c605e1b3
Fiore-Gartland <agartlan@fredhutch.org>
2018-03-08 20:55:44 -0800

    HVTN colors

---
+++ hvtn_colors.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+__all__ = ['HVTNcolors', 'HVTNrx', 'HVTNLU']
+
+HVTNcolors = np.array([(120, 120, 115),
+                       (23, 73, 255),
+                       (217, 35, 33),
+                       (10, 183, 201),
+                       (255, 111, 27),
+                       (129, 0, 148),
+                       (255, 94, 191),
+                       (143, 143, 143)])
+HVTNrx = ['C', 'T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'Negative']
+
+HVTNLU = {rx:c for rx,c in zip(HVTNrx, HVTNcolors)}
