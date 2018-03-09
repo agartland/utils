2953baf936848053477cb5c781c3c8e79e5c4272
Fiore-Gartland <agartlan@fredhutch.org>
2018-03-08 23:12:26 -0800

    Colors

--- hvtn_colors.py
+++ hvtn_colors.py
@@ -9,7 +9,7 @@ HVTNcolors = np.array([(120, 120, 115),
                        (255, 111, 27),
                        (129, 0, 148),
                        (255, 94, 191),
-                       (143, 143, 143)])
+                       (143, 143, 143)])/255.
 HVTNrx = ['C', 'T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'Negative']
 
 HVTNLU = {rx:c for rx,c in zip(HVTNrx, HVTNcolors)}
