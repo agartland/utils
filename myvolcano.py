cab87d770427b278c32eeeedfaa449d7dad56e50
Fiore-Gartland <agartlan@fredhutch.org>
2022-05-16 10:34:35 -0700

    Drafted a little function for volcano plots in altair

--- myvolcano.py
+++ myvolcano.py
@@ -6,6 +6,40 @@ import sys
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, AutoMinorLocator
 
+import altair as alt
+
+df = pd.DataFrame(dict(x=np.random.rand(20),
+                        y=np.random.normal(20),
+                        category=np.random.choice(['A','B', 'C'], size=20)))
+
+def altair_scatter(x, y, hue, data, tooltip=[], yscale='linear', xscale='linear', palette=None, size=60, stroke=2, fontsize=14, title=''):
+    # brush = alt.selection(type='single', resolve='global')
+    # palette = {'Neither':'black', 'Y only':'gold', 'X only':'blue', 'Both':'red'}
+    if palette is None and not hue is None:
+        # tmp = sns.color_palette('Set2',  n_colors=data[hue].unique().shape[0])
+        tmp = ['dodgerblue', 'tomato', 'black', 'green', 'eggplant']
+        palette = {h:c for h,c in zip(data[hue].unique(), tmp)}
+
+        col_dom = [c for c in palette]
+        col_rng = [palette[c] for c in palette]
+
+    brush = alt.selection_single(resolve='global')
+    base = alt.Chart(data).add_selection(brush).mark_point(size=size, strokeWidth=stroke).interactive()
+
+    if not palette is None:
+        tmp_color = alt.Color(field=hue, type='nominal', scale=alt.Scale(domain=col_dom, range=col_rng))
+        color_param = alt.condition(brush, tmp_color, alt.ColorValue('gray')),
+    else:
+        color_param = None
+
+    ch = base.encode(x=alt.Y(x, scale=alt.Scale(type=xscale)),
+                     y=alt.Y(y, scale=alt.Scale(type=yscale)),
+                     tooltip=tooltip,
+                     color=color_param).properties(title=title)
+    ch = ch.configure_title(fontSize=fontsize).configure_axis(labelFontSize=fontsize-2, titleFontSize=fontsize)
+    return ch
+
+
 
 def plot_volcano(df, pvalue_col, or_col, sig_col, ann_col=None, annotate=None, censor_or=None):
     
